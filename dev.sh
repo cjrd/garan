0301b60a6fa656980ac0643b7eb1b02c1273cf50
Colorado Reed <colorado.j.reed@gmail.com>
2020-01-15 01:08:42 +0000

    Small updates to dev script

--- dev.sh
+++ dev.sh
@@ -1,13 +1,16 @@
-HOMEDIR='/home/coloradojreed'
+#!/bin/bash
+
+set -e
+export HOMEDIR='/home/coloradojreed'
+export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 docker run \
        -v ${HOMEDIR}/Development:/root/kondo \
        -v ${HOMEDIR}/.zsh_history:/root/.zsh_history \
-       -v ${HOMEDIR}/.tmux.conf:/root/.tmux.conf \
+       -v ${DIR}/tmux.conf:/root/.tmux.conf \
        -v ${HOMEDIR}/.ssh:/root/.ssh \
-       -v ${HOMEDIR}/.zshrc:/root/.zshrc \
+       -v ${DIR}/zshrc:/root/.zshrc \
        -v ${HOMEDIR}/.emacs.d:/root/.emacs.d \
        --net=host \
        --name=${USER} \
        -it garan zsh
-
-
